300cc7abdf855c11f3edd5af3d1d6ef3b5a6a53f
Agnes Brettl <brettl1991@gmail.com>
2026-03-29 17:40:03 +0100

    computed properties and components and props

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,16 @@
 <script setup>
 import { ref } from "vue";
-import socksGreenImage from "./assets/images/socks_green.jpeg";
-import socksBlueImage from "./assets/images/socks_blue.jpeg";
+import ProductDisplay from "@/components/ProductDisplay.vue";
 
-const product = ref("Socks");
-const image = ref(socksGreenImage);
-const inStock = true;
-
-const details = ref(["50% cotton", "30% wool", "20% polyester"]);
-
-const variants = ref([
-  { id: 2234, color: "green", image: socksGreenImage },
-  { id: 2235, color: "blue", image: socksBlueImage },
-]);
-
-const cart = ref(0);
-
-const addToCart = () => (cart.value += 1);
-
-const updateImage = (variantImage) => (image.value = variantImage);
+const cart = ref([]);
+const premium = ref(true);
+const updateCart = (id) => {
+  cart.value.push(id);
+};
 </script>
 
 <template>
   <div class="nav-bar"></div>
-  <div class="cart">Cart({{ cart }})</div>
-  <div class="product-display">
-    <div class="product-container">
-      <div class="product-image">
-        <img v-bind:src="image" />
-      </div>
-      <div class="product-info">
-        <h1>{{ product }}</h1>
-        <p v-if="inStock">In Stock</p>
-        <p v-else>Out of Stock</p>
-        <ul>
-          <li v-for="detail in details">{{ detail }}</li>
-        </ul>
-        <div
-          v-for="variant in variants"
-          :key="variant.id"
-          @mouseover="updateImage(variant.image)"
-          class="color-circle"
-          :style="{ backgroundColor: variant.color }"
-        ></div>
-        <button class="button" v-on:click="addToCart">Add to cart</button>
-      </div>
-    </div>
-  </div>
+  <div class="cart">Cart({{ cart.length }})</div>
+  <ProductDisplay :premium="premium" @add-to-cart="updateCart"></ProductDisplay>
 </template>
